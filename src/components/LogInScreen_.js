4cfda0fd4947eae41a757d5f40d2078ce53af54a
Ferran Agulló López <fewrnap@gmail.com>
2019-05-27 11:10:31 +0200

    changes hostname

--- src/components/LogInScreen_.js
+++ src/components/LogInScreen_.js
@@ -40,7 +40,7 @@ class LogInScreen extends React.Component {
           <div className="py-5 text-center" style={{backgroundImage: 'url("https://static.pingendo.com/cover-bubble-dark.svg")', backgroundSize: 'cover', position: 'fixed', bottom: 0, left: 0, right: 0, top: 0}}>
             <div className="container" style={{paddingTop:"300px"}}>
                   <h1 style={{textAlign:"center"}}>Welcome to our Issue Tracker!</h1>
-                  <h3 style={{textAlign:"center"}}><a style={{color:"#fff", padding:"10px", backgroundColor:"#307bff", width:"50px", borderRadius:"5px"}} href={`https://blooming-forest-68248.herokuapp.com/sessions/login?sessionId=${this.state.sessionID}&callback=http://localhost:3000/`}>Login</a></h3>
+                  <h3 style={{textAlign:"center"}}><a style={{color:"#fff", padding:"10px", backgroundColor:"#307bff", width:"50px", borderRadius:"5px"}} href={`https://blooming-forest-68248.herokuapp.com/sessions/login?sessionId=${this.state.sessionID}&callback=https://reactjsissuetracker.herokuapp.com/`}>Login</a></h3>
             </div>
           </div>
 
